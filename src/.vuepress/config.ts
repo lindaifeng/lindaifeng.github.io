983c0ee6a7d7937a5bf0966d1b23c302bc031e05
ldf <1305366530@qq.com>
2024-10-22 15:23:29 +0800

    update 首页引入google统计

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -10,7 +10,14 @@ export default defineUserConfig({
   description: "vuepress-theme-hope 的文档演示",
 
   theme,
-
+  head:[
+      ['script', { src: "https://www.googletagmanager.com/gtag/js?id=G-Y1LWB9B9G4",async: true}],
+      ['script', {},
+        " window.dataLayer = window.dataLayer || [];\
+        function gtag(){dataLayer.push(arguments);}\
+        gtag('js', new Date());\
+        gtag('config', 'G-Y1LWB9B9G4');"],
+      ]
   // 和 PWA 一起启用
   // shouldPrefetch: false,
 });
