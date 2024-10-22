721b194d6b90007ee305cb5d03b9d7614085f9cc
ldf <1305366530@qq.com>
2024-10-22 15:34:09 +0800

    update 首页引入baidu统计

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -11,13 +11,22 @@ export default defineUserConfig({
 
   theme,
   head:[
-      ['script', { src: "https://www.googletagmanager.com/gtag/js?id=G-Y1LWB9B9G4",async: true}],
-      ['script', {},
-        " window.dataLayer = window.dataLayer || [];\
-        function gtag(){dataLayer.push(arguments);}\
-        gtag('js', new Date());\
-        gtag('config', 'G-Y1LWB9B9G4');"],
-      ]
+      // google统计分析
+      // ['script', { src: "https://www.googletagmanager.com/gtag/js?id=G-Y1LWB9B9G4",async: true}],
+      // ['script', {},
+      //   " window.dataLayer = window.dataLayer || [];\
+      //   function gtag(){dataLayer.push(arguments);}\
+      //   gtag('js', new Date());\
+      //   gtag('config', 'G-Y1LWB9B9G4');"],
+    //baidu统计
+    ['script',{},"var _hmt = _hmt || [];\n" +
+    "(function() {\n" +
+    "  var hm = document.createElement(\"script\");\n" +
+    "  hm.src = \"https://hm.baidu.com/hm.js?5fd4bcd39197e8c4bf406d1b2add643c\";\n" +
+    "  var s = document.getElementsByTagName(\"script\")[0]; \n" +
+    "  s.parentNode.insertBefore(hm, s);\n" +
+    "})();"]
+  ]
   // 和 PWA 一起启用
   // shouldPrefetch: false,
 });
