5030a4ccae7deb9d40ad76d118001602bda82629
lindaifeng <61862335+lindaifeng@users.noreply.github.com>
2024-10-25 11:20:37 +0800

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -4,7 +4,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/docs/",
+  base: "/",
   lang: "zh-CN",
   title: "文档演示",
   description: "vuepress-theme-hope 的文档演示",
